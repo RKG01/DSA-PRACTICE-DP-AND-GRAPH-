58f6effd63e8fc4c22d6763a332dbb712934398a
RKG01 <rk5849193@gmail.com>
2025-09-18 23:25:25 +0530

    leetcode 2115

--- graph/topoort.cpp
+++ graph/topoort.cpp
@@ -1,2 +1,53 @@
 // https://leetcode.com/submissions/detail/1774878030/
 
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ing,
+                              vector<string>& sup) {
+    unordered_map<string, vector<string>> graph;
+    int n = rec.size();
+    unordered_set<string> s;
+    for (auto x : sup)
+        s.insert(x);
+
+    unordered_map<string, int> indegree;
+    for (auto x : rec)
+        indegree[x] = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (int)ing[i].size(); j++) {
+            if (s.find(ing[i][j]) == s.end()) {
+                graph[ing[i][j]].push_back(rec[i]);
+                indegree[rec[i]]++;
+            }
+        }
+    }
+
+    queue<string> q;
+    for (auto x : indegree) {
+        if (x.second == 0) {
+            q.push(x.first);
+        }
+    }
+    vector<string> ans;
+    while (!q.empty()) {
+        string tmp = q.front();
+        q.pop();
+        ans.push_back(tmp);
+        for (auto nbr : graph[tmp]) {
+            indegree[nbr]--;
+            if (indegree[nbr] == 0)
+                q.push(nbr);
+        }
+    }
+    return ans;
+}
+};
